8abe1f5065c030ce617600376dfcd5c8dc7c2bd0
Elias Montan <eliasmontan10@gmail.com>
2025-07-19 19:54:51 -0300

    Initialize project

---
+++ PasswordGeneratorMenuBar/PasswordGeneratorMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorMenuBarApp.swift
+//  PasswordGeneratorMenuBar
+//
+//  Created by Malu Montan on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PasswordGeneratorMenuBarTests/PasswordGeneratorMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorMenuBarTests.swift
+//  PasswordGeneratorMenuBarTests
+//
+//  Created by Malu Montan on 7/19/25.
+//
+
+import Testing
+@testable import PasswordGeneratorMenuBar
+
+struct PasswordGeneratorMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
